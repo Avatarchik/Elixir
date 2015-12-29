3584454e12873192d5c60c5350595a4b57de27e9
Minjung Kim <Reny.atkins@gmail.com>
2015-12-29 20:53:32 +0900

    Make thermometer

---
+++ Assets/BattleTest/Thermometer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Thermometer : MonoBehaviour {
+
+    int currentTemperature;
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GameObject.Find("ThermometerText").GetComponent<Text>();
+        currentTemperature = 20; // default temperature.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "현재 온도 : " + currentTemperature + " 도";
+	}
+}
