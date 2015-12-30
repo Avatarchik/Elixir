0f7e0632a249b08a807864d53bbbd08a3f019e0c
Minjung Kim <Reny.atkins@gmail.com>
2015-12-30 17:15:12 +0900

    Make monster prefab - task 2009

---
+++ Assets/BattleTest/Monster.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MonsterType
+{
+    None
+}
+
+public class Monster : MonoBehaviour {
+
+    public new SpriteRenderer renderer;
+    public int hp;
+    public int attackDamage;
+    public MonsterType type;
+    public int boilingPoint;
+    public int meltingPoint;
+    // List<Card> cards; // Current not used.
+    
+    // Apply default stats.
+    public Monster ()
+    {
+        this.hp = 20;
+        this.attackDamage = 5;
+        this.type = MonsterType.None;
+        this.boilingPoint = 100;
+        this.meltingPoint = 0;
+        // this.cards = new List<Card>(); // Current not used.
+    }
+    
+    // Default image.
+    public Monster (int hp, int attackDamage, MonsterType type, int boilingPoint, int meltingPoint)
+    {
+        this.hp = hp;
+        this.attackDamage = attackDamage;
+        this.type = type;
+        this.boilingPoint = boilingPoint;
+        this.meltingPoint = meltingPoint;
+        // this.cards = new List<Card>(); // Current not used.
+    }
+    public Monster (string imagePath, int hp, int attackDamage, MonsterType type, int boilingPoint, int meltingPoint)
+    {
+        this.renderer.sprite = Resources.Load(imagePath, typeof(Sprite)) as Sprite;
+        this.hp = hp;
+        this.attackDamage = attackDamage;
+        this.type = type;
+        this.boilingPoint = boilingPoint;
+        this.meltingPoint = meltingPoint;
+        // this.cards = new List<Card>(); // Current not used.
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BattleTest/MonsterManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterManager : MonoBehaviour {
+
+    List<Vector2> positions = new List<Vector2>();
+    Vector2 firstPosition = new Vector2(2.6f, 0.6f);
+    Vector2 secondPosition = new Vector2(4.0f, -3.0f);
+    Vector2 thirdPosition = new Vector2(5.6f, 2.0f);
+    Vector2 fourthPosition = new Vector2(7.0f, 1.3f);
+
+    void MakePositionList()
+    {
+        positions.Add(firstPosition);
+        positions.Add(secondPosition);
+        positions.Add(thirdPosition);
+        positions.Add(fourthPosition);
+    }
+
+	// Use this for initialization
+	void Start () {
+        MakePositionList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
