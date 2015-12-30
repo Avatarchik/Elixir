338e5a97632e9fbd98075d8faaa992bdb449c581
bangduk63 <rkd4858@naver.com>
2015-12-30 17:16:42 +0900

    1009

--- Assets/myfolder/my_Scripts/CardChoice.cs
+++ Assets/myfolder/my_Scripts/CardChoice.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class CardChoice : MonoBehaviour {
+	public IEnumerator cardChoice(){
+		if(Input.GetMouseButtonDown(0)){
+			Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (pos,Vector2.zero,0f);
+			if(hit.collider!=null)
+			{
 
-	// Use this for initialization
-	void Start () {
-	
+			}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 }
