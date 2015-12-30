20ec324d03e8bc8c3531f0bd752dc13dbd524a0b
bangduk63 <rkd4858@naver.com>
2015-12-30 17:19:56 +0900

    1009'

--- Assets/myfolder/my_Scripts/CardChoice.cs
+++ Assets/myfolder/my_Scripts/CardChoice.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class CardChoice : MonoBehaviour {
-	public IEnumerator cardChoice(){
-		if(Input.GetMouseButtonDown(0)){
-			Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			RaycastHit2D hit = Physics2D.Raycast (pos,Vector2.zero,0f);
-			if(hit.collider!=null)
-			{
-
+	/*public IEnumerator cardChoice(){
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (pos, Vector2.zero, 0f);
+			if (hit.collider != null) {
+				Debug.Log (hit.transform.GetComponent<InfoCard> ().Card.Card_ExtName);
 			}
-
-	}
+			yield return;
+		}
+	}*/
 
 
 }
