060fbbc6d675481219ed478128423ea0684cf7ed
Minjung Kim <Reny.atkins@gmail.com>
2015-12-31 02:19:11 +0900

    Divide enums using 'Enums' namespace

---
+++ Assets/BattleTest/Enums.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Enums
+{
+    public enum MonsterType
+    {
+        None
+    }
+
+    public enum BuffName
+    {
+        None
+    }
+
+    public enum DebuffName
+    {
+        None,
+        Stun
+    }
+
+    public enum Phase
+    {
+        Solid,
+        Liquid,
+        Gas
+    }
+
+    public class Buff
+    {
+        BuffName name;
+        int remainTurn;
+        
+        public Buff (BuffName name, int remainTurn)
+        {
+            this.name = name;
+            this.remainTurn = remainTurn;
+        }
+    }
+
+    public class Debuff
+    {
+        BuffName name;
+        int remainTurn;
+        
+        public Debuff (BuffName name, int remainTurn)
+        {
+            this.name = name;
+            this.remainTurn = remainTurn;
+        }
+    }
+}
