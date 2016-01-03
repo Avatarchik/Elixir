9fded62bb144afc162f1a8a105abbc6cd7ae8007
bangduk63 <rkd4858@naver.com>
2016-01-04 02:29:43 +0900

    1017 generate Player in fixed position

---
+++ Assets/AllyManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllyManager : MonoBehaviour {
+
+	public GameObject PlayerPrefab;
+	Vector3 PlayerPosition = new Vector3 (-4.44f, 0.83f, 0f);
+	void Start () {
+		GeneratePlayer ();
+	}
+	
+
+	void GeneratePlayer () {
+		GameObject Player = Instantiate (PlayerPrefab);
+		Player.transform.position = PlayerPosition;
+	}
+}
