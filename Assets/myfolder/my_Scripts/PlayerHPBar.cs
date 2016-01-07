b63178bcebbbc71cb75216eb9126a32a9070a289
lyric01 <goddessxbell@gmail.com>
2016-01-07 20:03:37 +0900

    PlayerHPBar

---
+++ Assets/myfolder/my_Scripts/PlayerHPBar.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHPBar : MonoBehaviour {
+    public RectTransform healthTransform;
+    private float minXValue;
+    private float maxXValue;
+    private float currentHealth;
+    private float maxHealth;
+    //private float currentXValue;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(this.gameObject);
+        maxHealth = this.GetComponent<BaseCharacter>().MAX_HP;
+
+        maxXValue = healthTransform.localPosition.x;
+        Debug.Log("MaxXValue: " + maxXValue);
+        minXValue = healthTransform.localPosition.x - healthTransform.rect.width;
+        Debug.Log("MinXValue: " + minXValue);
+        Debug.Log("BarWidth: " + healthTransform.rect.width);
+        currentHealth = maxHealth;
+        HandleHealth();
+        //healthTransform.localPosition = new Vector2(minXValue, healthTransform.localPosition.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<BaseCharacter>().HP != currentHealth)
+        {
+            Debug.Log("HealthBar Change!! (Player)");
+            currentHealth = this.GetComponent<BaseCharacter>().HP;
+            HandleHealth();
+
+        }
+    }
+    private void HandleHealth()
+    {
+        float currentXValue = MapValues(currentHealth, 0, maxHealth, minXValue, maxXValue);
+        Debug.Log("Original XValue: " + healthTransform.localPosition.x + " Current XValue: " + currentXValue);
+
+        healthTransform.localPosition = new Vector2(currentXValue, healthTransform.localPosition.y);
+    }
+    private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
