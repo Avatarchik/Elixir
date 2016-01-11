bb75374a62854a1e64f2c4d01d6f5b3a7fd0ecba
lyric01 <goddessxbell@gmail.com>
2016-01-11 20:09:53 +0900

    MonsterDatabase Implemented

---
+++ Assets/myfolder/my_Scripts/MonsterLoad.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterLoad : MonoBehaviour {
+    public TextAsset file;
+    public List<baseMonster> monsterList = new List<baseMonster>();
+    List<string> row = new List<string>();
+    List<List<string>> rowList = new List<List<string>>();
+    bool isLoaded = false;
+    void Awake()
+    {
+        Debug.Log("Load Monste Database");
+        Load(file);
+        monsterList = new List<baseMonster>();
+        ListSetting();
+    }
+    public void Load(TextAsset csv)
+    {
+        rowList.Clear();
+        string[][] grid = CsvParser2.Parse(csv.text);
+        for (int i = 1; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                row.Add(grid[i][j]);
+            }
+            rowList.Add(row);
+        }
+        isLoaded = true;
+    }
+    void ListSetting()
+    {
+        for (int i = 0; i < rowList.Count; i++)
+        {
+            baseMonster monster = new baseMonster();
+            monster.Mon_ID = System.Convert.ToInt32(rowList[i][0]);
+            monster.Mon_ExtName = rowList[i][1];
+            monster.Mon_Name = rowList[i][2];
+            monster.Mon_Group = rowList[i][3];
+            monster.Mon_ChemicalSeries = rowList[i][4];
+            monster.Mon_Type = rowList[i][5];
+            monster.Mon_Grade = System.Convert.ToInt32(rowList[i][6]);
+            monster.Mon_Level = System.Convert.ToInt32(rowList[i][7]);
+            monster.Mon_AdventField = System.Convert.ToInt32(rowList[i][8]);
+            monster.Mon_HP = System.Convert.ToInt32(rowList[i][9]);
+            monster.Mon_AttackDamage = System.Convert.ToDouble(rowList[i][10]);
+            monster.Mon_Skill1_Name = rowList[i][11];
+            monster.Mon_Skill1_Rate = System.Convert.ToDouble(rowList[i][12]);
+            monster.Mon_Skill2_Name = rowList[i][13];
+            monster.Mon_Skill2_Rate = System.Convert.ToDouble(rowList[i][14]);
+            monster.Mon_CriticalTarget = rowList[i][15];
+            monster.Mon_RoomTempStatus = rowList[i][16];
+            monster.Mon_SolidGauge = System.Convert.ToInt32(rowList[i][17]);
+            monster.Mon_LiquidGauge = System.Convert.ToInt32(rowList[i][18]);
+            monster.Mon_GasGauge = System.Convert.ToInt32(rowList[i][19]);
+            monster.Mon_RoomTempPos = System.Convert.ToDouble(rowList[i][20]);
+            monster.Mon_MaxCount = System.Convert.ToInt32(rowList[i][21]);
+            monster.Mon_NotAppartionCond = rowList[i][22];
+            monster.Mon_GoldRate = System.Convert.ToInt32(rowList[i][23]);
+            monster.Mon_MonCard1GainRate = System.Convert.ToInt32(rowList[i][24]);
+            monster.Mon_MonCard2GainRate = System.Convert.ToInt32(rowList[i][25]);
+
+            monsterList.Add(monster);
+        }
+
+    }
+}
+
---
+++ Assets/myfolder/my_Scripts/baseMonster.cs
@@ -0,0 +1,369 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseMonster {
+    private int mon_ID;
+    private string mon_ExtName;
+    private string mon_Name;
+    private string mon_Group;
+    private string mon_ChemicalSeries;
+    private string mon_Type;
+    private int mon_Grade;
+    private int mon_Level;
+    private int mon_AdventField;
+    private int mon_HP;
+    private double mon_AttackDamage;
+    private string mon_Skill1_Name;
+    private double mon_Skill1_Rate;
+    private string mon_Skill2_Name;
+    private double mon_Skill2_Rate;
+    private string mon_CriticalTarget;
+    private string mon_RoomTempStatus;
+    private int mon_SolidGauge;
+    private int mon_LiquidGauge;
+    private int mon_GasGauge;
+    private double mon_RoomTempPos;
+    private int mon_MaxCount;
+    private string mon_NotAppartionCond;
+    private int mon_GoldRate;
+    private int mon_MonCard1GainRate;
+    private int mon_MonCard2GainRate;
+
+    public int Mon_ID
+    {
+        get
+        {
+            return mon_ID;
+        }
+
+        set
+        {
+            mon_ID = value;
+        }
+    }
+
+    public string Mon_ExtName
+    {
+        get
+        {
+            return mon_ExtName;
+        }
+
+        set
+        {
+            mon_ExtName = value;
+        }
+    }
+
+    public string Mon_Name
+    {
+        get
+        {
+            return mon_Name;
+        }
+
+        set
+        {
+            mon_Name = value;
+        }
+    }
+
+    public string Mon_Group
+    {
+        get
+        {
+            return mon_Group;
+        }
+
+        set
+        {
+            mon_Group = value;
+        }
+    }
+
+    public string Mon_ChemicalSeries
+    {
+        get
+        {
+            return mon_ChemicalSeries;
+        }
+
+        set
+        {
+            mon_ChemicalSeries = value;
+        }
+    }
+
+    public string Mon_Type
+    {
+        get
+        {
+            return mon_Type;
+        }
+
+        set
+        {
+            mon_Type = value;
+        }
+    }
+
+    public int Mon_Grade
+    {
+        get
+        {
+            return mon_Grade;
+        }
+
+        set
+        {
+            mon_Grade = value;
+        }
+    }
+
+    public int Mon_Level
+    {
+        get
+        {
+            return mon_Level;
+        }
+
+        set
+        {
+            mon_Level = value;
+        }
+    }
+
+    public int Mon_AdventField
+    {
+        get
+        {
+            return mon_AdventField;
+        }
+
+        set
+        {
+            mon_AdventField = value;
+        }
+    }
+
+    public int Mon_HP
+    {
+        get
+        {
+            return mon_HP;
+        }
+
+        set
+        {
+            mon_HP = value;
+        }
+    }
+
+    public double Mon_AttackDamage
+    {
+        get
+        {
+            return mon_AttackDamage;
+        }
+
+        set
+        {
+            mon_AttackDamage = value;
+        }
+    }
+
+    public string Mon_Skill1_Name
+    {
+        get
+        {
+            return mon_Skill1_Name;
+        }
+
+        set
+        {
+            mon_Skill1_Name = value;
+        }
+    }
+
+    public double Mon_Skill1_Rate
+    {
+        get
+        {
+            return mon_Skill1_Rate;
+        }
+
+        set
+        {
+            mon_Skill1_Rate = value;
+        }
+    }
+
+    public string Mon_Skill2_Name
+    {
+        get
+        {
+            return mon_Skill2_Name;
+        }
+
+        set
+        {
+            mon_Skill2_Name = value;
+        }
+    }
+
+    public double Mon_Skill2_Rate
+    {
+        get
+        {
+            return mon_Skill2_Rate;
+        }
+
+        set
+        {
+            mon_Skill2_Rate = value;
+        }
+    }
+
+    public string Mon_CriticalTarget
+    {
+        get
+        {
+            return mon_CriticalTarget;
+        }
+
+        set
+        {
+            mon_CriticalTarget = value;
+        }
+    }
+
+    public string Mon_RoomTempStatus
+    {
+        get
+        {
+            return mon_RoomTempStatus;
+        }
+
+        set
+        {
+            mon_RoomTempStatus = value;
+        }
+    }
+
+    public int Mon_SolidGauge
+    {
+        get
+        {
+            return mon_SolidGauge;
+        }
+
+        set
+        {
+            mon_SolidGauge = value;
+        }
+    }
+
+    public int Mon_LiquidGauge
+    {
+        get
+        {
+            return mon_LiquidGauge;
+        }
+
+        set
+        {
+            mon_LiquidGauge = value;
+        }
+    }
+
+    public int Mon_GasGauge
+    {
+        get
+        {
+            return mon_GasGauge;
+        }
+
+        set
+        {
+            mon_GasGauge = value;
+        }
+    }
+
+    public double Mon_RoomTempPos
+    {
+        get
+        {
+            return mon_RoomTempPos;
+        }
+
+        set
+        {
+            mon_RoomTempPos = value;
+        }
+    }
+
+    public int Mon_MaxCount
+    {
+        get
+        {
+            return mon_MaxCount;
+        }
+
+        set
+        {
+            mon_MaxCount = value;
+        }
+    }
+
+    public string Mon_NotAppartionCond
+    {
+        get
+        {
+            return mon_NotAppartionCond;
+        }
+
+        set
+        {
+            mon_NotAppartionCond = value;
+        }
+    }
+
+    public int Mon_GoldRate
+    {
+        get
+        {
+            return mon_GoldRate;
+        }
+
+        set
+        {
+            mon_GoldRate = value;
+        }
+    }
+
+    public int Mon_MonCard1GainRate
+    {
+        get
+        {
+            return mon_MonCard1GainRate;
+        }
+
+        set
+        {
+            mon_MonCard1GainRate = value;
+        }
+    }
+
+    public int Mon_MonCard2GainRate
+    {
+        get
+        {
+            return mon_MonCard2GainRate;
+        }
+
+        set
+        {
+            mon_MonCard2GainRate = value;
+        }
+    }
+}
