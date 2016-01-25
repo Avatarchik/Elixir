270617171c01b661223525e522f9b6eb0b34f749
lyric01 <goddessxbell@gmail.com>
2016-01-25 15:38:53 +0900

    AddTurn UI Implemented

---
+++ Assets/myfolder/my_Scripts/AddTurn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddTurn : MonoBehaviour {
+    float cooltime;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Cooldown Start");
+        cooltime = 2.0f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    cooltime -= Time.deltaTime;
+        if(cooltime < 0)
+        {
+            this.gameObject.SetActive(false);
+            cooltime = 2.0f;
+        }
+	}
+}
