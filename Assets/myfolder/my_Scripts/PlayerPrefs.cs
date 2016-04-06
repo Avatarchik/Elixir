4d4b6ccfd0b6597d0ef44b433d8a9b30253d39c6
Dagyeong <dmswk7215@naver.com>
2016-04-06 18:33:50 +0900

    quality up
    background quality upgrade

--- Assets/myfolder/my_Scripts/PlayerPrefs.cs
+++ Assets/myfolder/my_Scripts/PlayerPrefs.cs
@@ -80,7 +80,7 @@ public class PlayerPrefs : MonoBehaviour {
 
     public void GeneratePlayer()
     {
-		Vector3 PlayerPosition = new Vector3(-1.9f, .5f, 0);
+		Vector3 PlayerPosition = new Vector3(-2.9f, .7f, 0);
         GameObject Player = Instantiate(PlayerPrefab);
         Player.transform.position = PlayerPosition;
     }
