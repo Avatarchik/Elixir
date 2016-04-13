3d14ba0a3231db920d81c088f859c87ea546f9cc
nechwi <nechwi1121@naver.com>
2016-04-13 17:04:55 +0900

    몬스터 길게 클릭시 분석 판넬 오픈
    몬스터프리펩 -> MonsterEvent 스크립트 추가(몬스터관련 이벤트)
    GameManager -> AnalyzePanel 추가 (판넬을 관리함)

---
+++ Assets/myfolder/my_Scripts/AnalyzePanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalyzePanel : MonoBehaviour {
+	public GameObject analyzePanel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OpenAnalyzePanel(Monster analyzeMonster)
+	{
+		analyzePanel.SetActive (true);
+	}
+}
---
+++ Assets/myfolder/my_Scripts/MonsterEvent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterEvent : MonoBehaviour {
+
+	private int clickedMonsterID;
+	private Monster clickedMonster;
+	private bool checkPress;
+	private float timer;
+	void OnMouseDown()
+	{
+		clickedMonsterID = this.GetComponent<MonsterIndex> ().MonsterID;
+		clickedMonster = GameObject.Find("MonsterManager").GetComponent<MonsterPrefs>().monsterList[clickedMonsterID];
+		Debug.Log (clickedMonster.monsterExtName);
+		CheckLongPress();
+	}
+
+	void Update()
+	{
+		if (checkPress)
+		{
+			timer -= Time.deltaTime;
+			if(timer <= 0)
+			{
+				GameObject.Find("GameManager").GetComponent<AnalyzePanel>().OpenAnalyzePanel(clickedMonster);
+				Debug.Log("sadasdasd");
+				checkPress = false;
+			}
+		}
+	}
+
+	public void CheckLongPress()
+	{
+		checkPress = true;
+		timer = 1.5f;
+	}
+
+	public void OnMouseUp()
+	{
+		checkPress = false;
+	}
+}
