b2e7ee7af8c7edc0ee5a39f07bfc965cd42bdc94
nechwi <nechwi1121@naver.com>
2016-04-16 08:48:19 +0900

    버튼오브젝트 이름 수정

---
+++ Assets/myfolder/my_Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("클릭됨");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
